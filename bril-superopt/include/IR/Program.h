5de6033feec1d09b47ef32943e1703284410a7e1
Arthur-Chang016 <b05505016@g.ntu.edu.tw>
2025-01-07 21:31:06 -0800

    Add IR/Program backbone

---
+++ bril-superopt/include/IR/Program.h
@@ -0,0 +1,14 @@
+#ifndef IR_PROGRAM_H
+#define IR_PROGRAM_H
+
+namespace ir {
+
+class Program {
+   public:
+    Program();
+    ~Program();
+};
+
+}  // namespace ir
+
+#endif  // IR_PROGRAM_H
---
+++ bril-superopt/src/IR/Program.cpp
@@ -0,0 +1,8 @@
+#include <IR/Program.h>
+
+namespace ir {
+
+Program::Program() {
+}
+
+}  // namespace ir
